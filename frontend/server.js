4ef8ceb71001c6206815d0e21a8da6404b6ae7ff
Tyler McGue <tylermcgue@gmail.com>
2025-03-24 23:32:14 -0700

    Reconfigured server.js to be friendly with how it is parsed

--- frontend/server.js
+++ frontend/server.js
@@ -1,22 +1,29 @@
-import { createServer } from 'next';
+import next from 'next';
 import { parse } from 'url';
-import http from'https';
+import { createServer as createHttpServer } from'https';
 
 const port = process.env.FRONTEND_PORT || 3000;
 const hostname = '0.0.0.0';
 
-const app = createServer({
+const app = next({
     dev: false,
 });
 
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-    http.createServer((req, res) => {
-        const parsedUrl = parse(req.url, true);
-        handle(req, res, parsedUrl);
-    }).listen(port, hostname, (err) => {
-        if (err) throw err;
-        console.log(`Next.js running on ${hostname}:${port}`);
+async function start() {
+    await app.prepare().then(() => {
+        createHttpServer((req, res) => {
+            const parsedUrl = parse(req.url, true);
+            handle(req, res, parsedUrl);
+        }).listen(port, hostname, (err) => {
+            if (err) throw err;
+            console.log(`Next.js running on ${hostname}:${port}`);
+        })
     })
+}
+
+start().catch((err) => {
+    console.error(err);
+    process.exit(1);
 })
